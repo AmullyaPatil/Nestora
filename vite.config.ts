ff370271980b171312b5dcb2d3594fa4905f0341
AmullyaPatil <patilamullya@gmail.com>
2025-03-13 18:50:55 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -14,8 +14,8 @@ export default defineConfig(({ mode }) => ({
     mode === "development" && componentTagger(),
   ].filter(Boolean),
 
-  // base: "/Nestora/",
-  base: "./",
+  base: "/Nestora/",
+  // base: "./",
 
   
   resolve: {
